e373f6f297ddb9253ba304e68bdec2e616919972
guiferrini <guilhermemferrini@gmail.com>
2020-03-16 17:10:48 -0300

    First API with Vue.js and Axios. Find Movie with TMDb

---
+++ my-project/src/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <h1>Search</h1>
+    <input type='text' v-model="query" @keyup="getResult(query)">
+    <div v-for='result in results' :key='result.id'>
+   <p>{{result.title}}</p>
+   <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.poster_path" width='100px'>
+  </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'search',
+  data () {
+    return {
+        query: '',
+        results: ''
+    }
+  },
+  methods: {
+    getResult(query) {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=cea68b520beecac6718820e4ac576c3a&language=es-ES&query=${query}`).then(response => { this.results = response.data.results })
+            console.log(this.results)
+    }
+ }
+}
+</script>
